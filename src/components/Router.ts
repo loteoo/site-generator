303f1a0c1be01ff84be126cecb00355fac2bf7e2
Alexandre Lotte <alexandre.lotte@osedea.com>
2021-03-18 00:38:34 -0400

    Reuse existing HTML while loading page

--- src/components/Router.ts
+++ src/components/Router.ts
@@ -1,4 +1,5 @@
 import { h, text } from 'hyperapp'
+import htmlToVdom from '../htmlToVdom';
 import { ViewContext } from '../types';
 
 /**
@@ -26,19 +27,39 @@ const Router = () => ({ state, meta, options }: ViewContext) => {
         });
       }
 
-      return view(state)
+      return (
+        h('div', { id: 'router-outlet' }, [
+          view(state)
+        ])
+      )
     }
+  }
+
+  // Check if a prerendered piece of HTML can be reused while JS / JSON loads
+  const previousOutlet = document.getElementById('router-outlet')
+  if (previousOutlet) {
+    const node = htmlToVdom(previousOutlet.innerHTML)
+    node.tag = 'div'
+    node.props = { id: 'router-outlet' }
+    return node
   }
 
   // Display custom loader if specified
   if (options.loader && typeof options.loader === 'function') {
-    return options.loader(state)
+    return (
+      h('div', { id: 'router-outlet' }, [
+        options.loader(state)
+      ])
+    )
   }
+
 
   // Default loader
   return (
-    h('div', { style: { padding: '1rem' } }, [
-      h('h2', { style: { textAlign: 'center' } }, text('Loading...'))
+    h('div', { id: 'router-outlet' }, [
+      h('div', { style: { padding: '1rem' } }, [
+        h('h2', { style: { textAlign: 'center' } }, text('Loading...'))
+      ])
     ])
   )
 }
---
+++ src/htmlToVdom.ts
@@ -0,0 +1,45 @@
+import { h, text } from 'hyperapp'
+
+/**
+ * Html to hyperapp VDOM converter
+ * Someone should make this a package...
+ */
+
+const mapProps = attrs => (
+  [...attrs].reduce(
+    (props, attr) => (
+      attr.nodeName === 'style'
+        ? props // ignore string style definitions for now.
+        : { ...props, [attr.nodeName]: attr.nodeValue }
+    ),
+    {}
+  )
+)
+
+const mapChildren = (childNodes) => {
+  if (!!childNodes && childNodes.length > 0) {
+    return [...childNodes].map(node => mapVNode(node))
+  } else {
+    return []
+  }
+}
+
+const mapVNode = (node) => {
+  switch (node.nodeType) {
+    case Node.TEXT_NODE:
+      return text(node.nodeValue)
+    case Node.ELEMENT_NODE:
+      return h(node.tagName, mapProps(node.attributes), mapChildren(node.childNodes))
+    default:
+      throw new Error(`${node.nodeType} is not supported`)
+  }
+}
+
+const htmlToVdom = (html) => {
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(html, 'text/html')
+  const node = mapVNode(doc.body)
+  return node
+}
+
+export default htmlToVdom
