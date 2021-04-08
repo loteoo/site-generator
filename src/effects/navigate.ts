26b163991528c084eecbce10ed9b183a18f79c97
Alexandre Lotte <alexandre.lotte@osedea.com>
2021-04-07 20:42:17 -0400

    Remove automatic scroll to top

--- src/effects/navigate.ts
+++ src/effects/navigate.ts
@@ -6,9 +6,6 @@ import fx from '../utils/fx'
 const navigate = fx((_dispatch, to) => {
   history.pushState(null, '', to)
   dispatchEvent(new CustomEvent("pushstate"))
-  window.scroll({
-    top: 0
-  })
 })
 
 export default navigate
